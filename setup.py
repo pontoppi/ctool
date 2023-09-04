4e81d5c36e0a7a3d49cd2ca04e967ff0aa1aa5ff
Klaus Pontoppidan <klausmp@mt-501727.lan>
2023-09-04 16:51:17 -0700

    update email address

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='ctool',
       version=1.0,
       description='Tool to estimate the continuum of a line spectrum observed with JWST',
-      author='Klaus Pontoppidan (STScI)',
-      author_email='pontoppi@stsci.edu',
+      author='Klaus Pontoppidan (JPL)',
+      author_email='klaus.m.pontoppidan@jpl.nasa.gov',
       download_url = '',
       packages=['ctool'],
       install_requires=[]
